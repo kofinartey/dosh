74d91d6560083c9b866ec317c7ad22640151a320
Kofi Nartey <eugeneahulu15@live.com>
2022-01-10 01:13:20 +0000

    built onboarding page

---
+++ src/app/onboarding.js
@@ -0,0 +1,13 @@
+const dashMenu = document.querySelector(".dash-menu");
+
+let dashLinks = Array.from(dashMenu.children);
+let pagePath = window.location.pathname;
+dashLinks.forEach((link) => {
+  if (pagePath.includes(link.id)) {
+    console.log(link);
+    link.style.color = "black";
+    // link.style.backgroundColor = "green";
+    link.classList.add("current");
+  }
+});
+console.log(window.location.pathname);
