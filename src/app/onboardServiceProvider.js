f5359b404c541ad4fa6965a1f9fc75c7fa073d45
Kofi Nartey <eugeneahulu15@live.com>
2022-01-13 23:03:03 +0000

    initialized onboarding corporate

--- src/app/onboardServiceProvider.js
+++ src/app/onboardServiceProvider.js
@@ -1,4 +1,23 @@
 const arrows = document.querySelectorAll(".arrow");
-const navRadios = document.querySelectorAll("foundation-radios");
-console.log(arrows);
-console.log(navRadios);
+const navRadios = document.querySelectorAll(".foundation-radios");
+let index = 0;
+
+arrows[0].addEventListener("click", () => {
+  if (index > 0) index--;
+  navRadios[index].checked = true;
+});
+arrows[1].addEventListener("click", () => {
+  if (index < navRadios.length - 1) index++;
+  navRadios[index].checked = true;
+});
+
+// if (index === 0) {
+//   arrows[0].style.opacity = "0";
+// } else if (index === navRadios.length - 1) {
+//   arrows[1].style.opacity = "0";
+// } else {
+//   arrows[0].style.opacity = "1";
+//   //   arrows.forEach((arrow) => {
+//   //     arrow.style.opacity = "1";
+//   //   });
+// }
